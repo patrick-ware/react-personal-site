3ad4f0f7f4699e3ed03e8d71bce20a6baf88fc6e
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-05 12:51:58 -0700

    icons added, need to figure out background & making it round

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
 import Headshot from './static/Headshot_Color.JPG'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+library.add(fab, fas)
 
 function App() {
   return (
@@ -23,7 +28,14 @@ function App() {
         </div>
         <div className="Page-About-Container">
 				  <img className="Headshot" src={Headshot}/>
-          <div className="Page-About-Body"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>
+          <div className="Page-About-Title-Bar">
+            <div className="Page-About-Greeting">Hi, I'm Patrick!</div>
+            <FontAwesomeIcon className="Icon" icon={['fas', 'circle','stack-2x']} />
+            <FontAwesomeIcon className="Icon" icon={['fab', 'github']} />
+            <FontAwesomeIcon className="Icon" icon={['fab', 'linkedin-in']} />
+            <FontAwesomeIcon className="Icon" icon={['fas' , 'file-pdf']} />
+          </div>
+          <div className="Page-About-Bio"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>
         </div>
       </div>
     </div>
