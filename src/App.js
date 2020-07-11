b07b5c660c529e10636406721ef2fdc2c85d7efc
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-11 12:04:02 -0700

    nav bar adjusting, comparing results of modifying bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
 import NavBar from './components/NavBar/NavBar.js'
 import AboutMe from './components/AboutMe/AboutMe.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,19 +2,37 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 import './NavBar.css';
 
 function NavBar(props) {
   return (
   <div>
-      <Navbar className="NavBar">
-          <Nav className="NavBar-Container">
-            <Nav.Link style={{color: "white"}} className="NavBar-Link" href="#home">Home</Nav.Link>
-            <Nav.Link className="NavBar-Link" href="#link">About Me</Nav.Link>
-            <Nav.Link className="NavBar-Link" href="#link">Projects</Nav.Link>
-            <Nav.Link className="NavBar-Link" href="#link">Contact</Nav.Link>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+  <Navbar className="NavBar" expand="lg">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About Me</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
+          </Navbar.Collapse>
       </Navbar>
+
   <div className="NavBar">
     <div className="NavBar-Container">
       <a className="NavBar-Link" href="#home">Home</a>
