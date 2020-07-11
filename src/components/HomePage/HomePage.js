81c04d49663635a225646a7926df1b720aee261c
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-11 09:43:41 -0700

    about me scroll link added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 import './HomePage.css';
 import BigSur from '../../static/BigSur.jpg'
+import AboutMe from '../AboutMe/AboutMe.js'
+
+function componentScroll(){
+  window.scrollTo({
+    top: 1000,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
 
 function HomePage(props) {
   return (
   <div>
+    <BrowserRouter>
     <div className="Page-Home">
       <img 
         className="Page-Home-Img"
@@ -17,12 +28,16 @@ function HomePage(props) {
       Oakland-based software engineer
       </div>
         <div className="Page-Home-Container">
-          <div className="Page-Home-Button"> About Me </div>
+        <Link to="#aboutme" className="Page-Home-Button" onClick={componentScroll}> About Me </Link> 
           <div className="Page-Home-Button"> Projects </div>
           <div className="Page-Home-Button"> Contact </div>
+            <Switch>
+              <Route exact path='#aboutme' component={AboutMe} />
+            </Switch>
+        </div>
       </div>
+     </BrowserRouter>
     </div>
-  </div>
   );
 }
 
