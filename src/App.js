c63998576af94ee0fcc76858b1ed953fe3dd4acf
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-11 11:40:40 -0700

    bootstrap navbar moved to component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+
 import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
 import NavBar from './components/NavBar/NavBar.js'
 import AboutMe from './components/AboutMe/AboutMe.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   return (
     <div>
       <HomePage />
-      <Navbar bg="dark" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
       <NavBar />
       <AboutMe />
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import './NavBar.css';
 
 function NavBar(props) {
   return (
+  <div>
+      <Navbar className="NavBar">
+          <Nav className="NavBar-Container">
+            <Nav.Link style={{color: "white"}} className="NavBar-Link" href="#home">Home</Nav.Link>
+            <Nav.Link className="NavBar-Link" href="#link">About Me</Nav.Link>
+            <Nav.Link className="NavBar-Link" href="#link">Projects</Nav.Link>
+            <Nav.Link className="NavBar-Link" href="#link">Contact</Nav.Link>
+          </Nav>
+      </Navbar>
   <div className="NavBar">
     <div className="NavBar-Container">
       <a className="NavBar-Link" href="#home">Home</a>
@@ -10,6 +22,7 @@ function NavBar(props) {
       <a className="NavBar-Link" href="#projects">Projects</a>
       <a className="NavBar-Link" href="#contact">Contact</a>
     </div>
+  </div>
   </div>
   );
 }
