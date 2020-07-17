efe5c1a826281a5f20328bc7b2893f399a845014
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-16 18:35:05 -0700

    sticky nav bar working!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { StickyContainer, Sticky } from 'react-sticky';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { StickyContainer, Sticky } from 'react-sticky'
 import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
 import NavBar from './components/NavBar/NavBar.js'
@@ -14,22 +14,23 @@ function App() {
       <HomePage />
       <StickyContainer>
         <Sticky> 
-          {({ 
-            style }) => (
-              <NavBar />
-            )}
+          {({ style }) => (
+            <div style={style}>
+              <NavBar/>
+            </div>
+          )}
         </Sticky>
-      </StickyContainer>
       <AboutMe />
       <Projects />
       <div className="Page-Contact">
         <div className="Page-Contact-Title">
           Contact Me
         </div>
-        <textarea>
-        Test contact form
-        </textarea>
+<textarea>
+Test contact form
+</textarea>
       </div>
+      </StickyContainer>
     </div>
   );
 }
