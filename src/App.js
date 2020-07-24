56a169974fd74a1afe30ae1ca08a4450af37c914
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-23 19:31:02 -0700

    contact component created

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import HomePage from './components/HomePage/HomePage.js'
 import NavBar from './components/NavBar/NavBar.js'
 import AboutMe from './components/AboutMe/AboutMe.js'
 import Projects from './components/Projects/Projects.js'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-library.add(fab, fas)
+import Contact from './components/Contact/Contact.js'
 
 function App() {
   return (
@@ -24,63 +18,13 @@ function App() {
         <Sticky> 
           {({ style }) => (
             <div style={style}>
-              <NavBar
-              />
+              <NavBar/>
             </div>
           )}
         </Sticky>
       <AboutMe />
       <Projects />
-      <div className="Page-Contact" id="contact">
-        <div className="Page-Contact-Title">
-          Contact Me
-        </div>
-        <div className="Page-Contact-Container">
-          <div className="Page-Contact-Text">
-   I'm currently seeking new opportunities in software engineering. If you are interested in working with me, or just want to code, the best way to get in contact is via email:
-          </div>
-        <div className="Page-Contact-Email">
-          patrick.r.ware@gmail.com
-        </div>
-        <div className="Links-Contact">
-          <a 
-            className="Icon-Container-Contact" 
-            href="mailto:patrick.r.ware@gmail.com & subject=Reaching out"
-          >
-            <FontAwesomeIcon 
-              className="Icon" 
-              icon={['fas', 'envelope']} 
-            />
-          </a>
-          <a 
-            className="Icon-Container-Contact" 
-            href="https://github.com/settings/profile"
-            target="_blank"
-          >
-            <FontAwesomeIcon 
-              className="Icon" 
-              icon={['fab', 'github']} 
-            />
-          </a>
-        <a 
-          className="Icon-Container-Contact"
-          href="https://www.linkedin.com/in/patrick-ware/"
-          target="_blank"
-        >
-          <FontAwesomeIcon 
-            className="Icon" 
-            icon={['fab', 'linkedin-in']} 
-          />
-        </a>
-        <div className="Icon-Container-Contact">
-          <FontAwesomeIcon 
-            className="Icon" 
-            icon={['fas' , 'file-pdf']} 
-          />
-        </div>
-      </div>
-      </div>
-    </div>
+      <Contact />
   </StickyContainer>
 </div>
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Contact.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(fab, fas)
+
+function Contact(props) {
+  return (
+  <div>
+      <div className="Page-Contact" id="contact">
+        <div className="Page-Contact-Title">
+          Contact Me
+        </div>
+        <div className="Page-Contact-Container">
+          <div className="Page-Contact-Text">
+   I'm currently seeking new opportunities in software engineering. If you are interested in working with me, or just want to code, the best way to get in contact is via email:
+          </div>
+        <div className="Page-Contact-Email">
+          patrick.r.ware@gmail.com
+        </div>
+        <div className="Links-Contact">
+          <a 
+            className="Icon-Container-Contact" 
+            href="mailto:patrick.r.ware@gmail.com & subject=Reaching out"
+          >
+            <FontAwesomeIcon 
+              className="Icon" 
+              icon={['fas', 'envelope']} 
+            />
+          </a>
+          <a 
+            className="Icon-Container-Contact" 
+            href="https://github.com/settings/profile"
+            target="_blank"
+          >
+            <FontAwesomeIcon 
+              className="Icon" 
+              icon={['fab', 'github']} 
+            />
+          </a>
+        <a 
+          className="Icon-Container-Contact"
+          href="https://www.linkedin.com/in/patrick-ware/"
+          target="_blank"
+        >
+          <FontAwesomeIcon 
+            className="Icon" 
+            icon={['fab', 'linkedin-in']} 
+          />
+        </a>
+        <div className="Icon-Container-Contact">
+          <FontAwesomeIcon 
+            className="Icon" 
+            icon={['fas' , 'file-pdf']} 
+          />
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Contact;
