a1d77766484964441ffaa5346a194c650d2ea3c8
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-30 23:08:26 -0700

    navbar component removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StickyContainer, Sticky } from 'react-sticky';
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
-import NavBar from './components/NavBar/NavBar.js'
 import AboutMe from './components/AboutMe/AboutMe.js'
 import Projects from './components/Projects/Projects.js'
 import Contact from './components/Contact/Contact.js'
