048cb935fcb2955da16f80f8995c8e14a06d2911
Patrick Ware <patrick.r.ware@gmail.com>
2020-07-31 08:25:14 -0700

    hamburger menu added to nav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import AboutMe from './components/AboutMe/AboutMe.js'
 import Projects from './components/Projects/Projects.js'
 import Contact from './components/Contact/Contact.js'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(fab, fas)
+
 function App() {
+  //set navbar menu visibility
+  const[showMenu, setShowMenu]=useState(false)
   return (
     <div>
       <HomePage/>
@@ -16,6 +25,12 @@ function App() {
           <a className="NavBar-Link" href="#about">About Me</a>
           <a className="NavBar-Link" href="#projects">Projects</a>
           <a className="NavBar-Link" href="#contact">Contact</a>
+          <a className="NavBar-Link" href="#">
+            <FontAwesomeIcon 
+              className="Icon" 
+              icon={['fas', 'bars']} 
+            />
+          </a>
         </div>
       </div>
       <AboutMe />
