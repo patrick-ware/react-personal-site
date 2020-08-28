c170ee138fd436b5a2123905fc09d69d3530edc3
Patrick Ware <patrick.r.ware@gmail.com>
2020-08-27 18:48:34 -0700

    code cleanup of unused functions and jsx

--- src/App.js
+++ src/App.js
@@ -32,35 +32,9 @@ function App() {
     setTimeout(toggleMenu, 400);
   }
 
-//   useEffect(() => {
-//      const timer = setTimeout(() => {
-//        toggleMenu('menu toggled');
-//      }, 1000);
-//      return () => clearTimeout(timer);
-//    },[]);
-
   return (
     <div>
       <HomePage/>
-      {/*ORIGINAL Bootstrap Navbar */}
-      {/*<Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>*/}
-      {/*Bootstrap Navbar */}
       <Navbar
         expanded={showMenu}
         onSelect={menuDelay}
